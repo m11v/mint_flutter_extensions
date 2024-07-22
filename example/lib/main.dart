762db4a24be2728a8bead361536ca7f1ee8fbfb2
irene.yu <irene.xin.yu@gmail.com>
2024-07-22 16:17:54 -0700

    Add extensions for TextStyle

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -44,7 +44,70 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        alignment: Alignment.topCenter,
+        child: ListView(
+          children: [
+            Text(
+              'This is a normal text',
+              style: context.body1,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is an italic text',
+              style: context.body1.italic,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is a bold text',
+              style: context.body1.bold,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is an underlined text',
+              style: context.body1.underline,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is a line-through text',
+              style: context.body1.lineThrough,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is an over lined text',
+              style: context.body1.overline,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is a tined text with underline',
+              style: context.body1.underline.tint(context.errorColor),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'This is a tined text with untined underline',
+              style: context.body1.underline.tint(
+                context.primaryColor,
+                false,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
