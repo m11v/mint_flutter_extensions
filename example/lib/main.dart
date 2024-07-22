f589ac2fe371df4c0a896f7ac459744304b19f37
irene.yu <irene.xin.yu@gmail.com>
2024-07-21 17:12:14 -0700

    Add an example app

---
+++ example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:mint_flutter_extensions/mint_flutter_extensions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Example of Mint Flutter Extensions',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: context.primaryColor,
+        title: Text(
+          title,
+          style: TextStyle(
+            color: context.onPrimaryColor,
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
