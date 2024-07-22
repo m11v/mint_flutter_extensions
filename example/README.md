d9cd9edfd7ae9a1eda566ed7c80222850cea7901
Irene Yu <irene.xin.yu@gmail.com>
2024-07-21 17:20:52 -0700

    Add an example app (#4)
    * Add an example app
    
    * update

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# Example of Mint Flutter Extensions
+
+This project is the example app of Mint Flutter Extensions.
---
+++ example/android/app/src/main/kotlin/com/mintminter/mint_flutter_extensions/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mintminter.mint_flutter_extensions.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
